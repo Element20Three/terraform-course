29d476addf5a3f5e314fe8927335b0c164e83bda
Vanadium <nvanadium@lm5000031238.local>
2017-11-06 14:55:47 -0500

    replacing vars

--- demo-4/vars.tf
+++ demo-4/vars.tf
@@ -1,11 +1,11 @@
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "us-east-1"
 }
 variable "AMIS" {
   type = "map"
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-844e0bf7"
+    us-east-1 = "ami-da05a4a0"
+    us-west-2 = "ami-0a00ce72"
+    eu-west-1 = "ami-add175d4"
   }
 }
