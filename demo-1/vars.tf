452d58f6e9a1081eb3f7ab1bad9f0de6a3f18f1f
Vanadium <nvanadium@lm5000031238.local>
2017-11-06 11:46:50 -0500

    edited variables

--- demo-1/vars.tf
+++ demo-1/vars.tf
@@ -1,13 +1,13 @@
-variable "AWS_ACCESS_KEY" {}
-variable "AWS_SECRET_KEY" {}
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
 variable "AWS_REGION" {
   default = "us-east-1"
 }
 variable "AMIS" {
   type = "map"
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-0d729a60"
+    us-east-1 = "ami-910faeeb"
+    us-west-1 = "ami-1bde7a62"
+    us-west-2 = "ami-0d1fd175"
   }
 }
